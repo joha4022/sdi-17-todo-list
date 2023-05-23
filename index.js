ccd97510ccd13070869dd29cda2049900ba16e61
joha4022 <joha4022@gmail.com>
2023-05-23 10:31:55 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,77 @@
+// displays current date
+const newDate = new Date();
+
+const year = newDate.getFullYear();
+const month = newDate.getMonth();
+const date = newDate.getDate();
+const day = newDate.getDay();
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+document.getElementById("current-date").innerHTML = `${weekday[day]} &nbsp &nbsp &nbsp ${month} / ${date} / ${year}`;
+
+// inputbox event listener and create
+const inputbox = document.querySelector('.input-box');
+const todoList = document.querySelector('ol');
+const addList = document.createElement('li');
+
+    // if the first list item is still display example then remove
+inputbox.addEventListener('input', (event) => {
+    if(todoList.children[0].id === 'example-todo') {
+        todoList.removeChild(todoList.children[0]);
+        todoList.appendChild(addList);
+        todoList.children[0].id = `list-${todoList.children.length}`
+    } else {
+        // else create a new li and display the typing list
+        todoList.children[todoList.children.length-1].style.display = '';
+        todoList.children[todoList.children.length-1].innerHTML = event.target.value;
+    }
+});
+
+// event listener for submit button and remove default action. When the submit
+// button is pressed, it creates a checkmark box.
+const submitButton = document.querySelector('.submit-button');
+const checkbox = ` <input type="checkbox" class="box" onclick="strike()">`;
+
+submitButton.addEventListener('click', (event) => {
+    if(inputbox.value.length > 0 ) {
+        todoList.children[todoList.children.length-1].innerHTML += checkbox;
+        todoList.children[todoList.children.length-1].children[0].id = `${todoList.children.length}`;
+        inputbox.value = '';
+        todoList.appendChild(document.createElement('li'));
+        todoList.children[todoList.children.length-1].style.display = 'none';
+        todoList.children[todoList.children.length-1].id = `list-${todoList.children.length}`;
+    }
+});
+
+// when enter is pressed, execute the same event.
+inputbox.addEventListener('keypress', (event) => {
+    if(inputbox.value.length > 0 && event.key === 'Enter') {
+        todoList.children[todoList.children.length-1].innerHTML += checkbox;
+        todoList.children[todoList.children.length-1].children[0].id = `${todoList.children.length}`;
+        inputbox.value = '';
+        todoList.appendChild(document.createElement('li'));
+        todoList.children[todoList.children.length-1].style.display = 'none';
+        todoList.children[todoList.children.length-1].id = `list-${todoList.children.length}`;
+    }
+});
+
+// reset button
+const resetButton = document.querySelector('#reset-button');
+
+resetButton.addEventListener('click', (event) => {
+    if(todoList.children[0].id !== 'example-todo') {
+        todoList.innerHTML = `<li id="example-todo">To do... <input type="checkbox" class="box"></li>`
+    }
+});
+
+// when the checkbox is checked, strikes through the li on the same level
+const strike = () => {
+    const boxes = document.querySelectorAll('.box');
+    for(let item of boxes) {
+        if(item.checked) {
+            todoList.children[item.id-1].style.textDecoration = 'line-through';
+        } else {
+            todoList.children[item.id-1].style.textDecoration = '';
+        }
+    }
+}
